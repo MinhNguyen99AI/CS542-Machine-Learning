3ed75dde77d3e1141726a25aeb219586875bdbc1
MINH LE NGUYEN <minhchanh991@gmail.com>
2022-01-31 12:50:55 -0500

    CS542 Machine Learning

---
+++ challenge/class_challenge/Main/model_7412.py
@@ -0,0 +1,42 @@
+from typing import Tuple
+import numpy as np
+
+
+class Model:
+    # Modify your model, default is a linear regression model with random weights
+    ID_DICT = {"MINH LE NGUYEN": "", "BU_ID": "U02407412", "BU_EMAIL": "minhng99@bu.edu"}
+
+    def __init__(self):
+        self.theta = None
+
+    def preprocess(self, X: np.array, y: np.array) -> Tuple[np.array, np.array]:
+        ###############################################
+        ####      add preprocessing code here      ####
+        ###############################################
+        return X, y
+
+    def train(self, X_train: np.array, y_train: np.array):
+        """
+        Train model with training data
+        """
+        ###############################################
+        ####   initialize and train your model     ####
+        ###############################################
+        #X_train = np.vstack((np.ones((X_train.shape[0],)), X_train.T)).T
+        #self.theta = np.random.rand(X_train.shape[1], 1)
+        
+        #Normal Equations with Regularlization (X^T X + lambda*I)X^T y
+        lambda_ = 4.1555
+        firstTerm = X_train.T.dot(X_train)
+        regularized = lambda_ * np.eye(X_train.shape[1])
+        self.theta = np.linalg.inv((firstTerm + regularized)).dot(X_train.T).dot(y_train)
+
+    def predict(self, X_val: np.array) -> np.array:
+        """
+        Predict with model and given feature
+        """
+        ###############################################
+        ####      add model prediction code here   ####
+        ###############################################
+        #X_val = np.vstack((np.ones((X_val.shape[0],)), X_val.T)).T
+        return np.dot(X_val, self.theta)
